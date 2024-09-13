58225706084647eb84ee1ee390cda639ccb51079
Mithila <100818591+mthlpbs@users.noreply.github.com>
2024-09-13 23:29:06 +0530

    robust login & add homepage
    Modify the login
    Create the homepage file

---
+++ src/ILS/Log.java
@@ -0,0 +1,59 @@
+package ILS;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Mithila Prabashwara
+ */
+public class Log {
+    
+    private static File logFile;
+
+    // Use to initialize the log file
+    public static void iniLogFile() {
+        String usrDir = System.getProperty("user.home");
+        String appDir = usrDir + "\\.ExtersILS";
+        File dir = new File(appDir);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        LocalDate dateD = LocalDate.now();
+        LocalTime timeD = LocalTime.now();
+        DateTimeFormatter timeFormatted = DateTimeFormatter.ofPattern("HH-mm-ss");
+        String formattedTime = timeD.format(timeFormatted);
+        logFile = new File(appDir + "\\" + "session " + dateD +"@" + formattedTime + ".txt");
+    }
+
+    // Use to write log statements
+    public static void write(String statement) {
+        LocalDate dateD = LocalDate.now();
+        String date = dateD.toString();
+        LocalTime timeD = LocalTime.now();
+        DateTimeFormatter timeFormatted = DateTimeFormatter.ofPattern("HH:mm:ss");
+        String time = timeD.format(timeFormatted);
+        try {
+            if (logFile == null) {
+                iniLogFile();
+            }
+            FileWriter txtEditor = new FileWriter(logFile, true);
+            txtEditor.write("["+date +"|"+ time +"]"+ " - " + statement + "\n");
+            txtEditor.close();
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "An unexpected error occurred. Contact your administrator.\nADMINREF:An IO error occurred while creating/updating the log file.\n"+"ERR MEG -"+e.getMessage());
+            System.exit(0);
+        }
+    }
+
+    // Main method
+    public static void main(String[] args) {
+        iniLogFile();
+    }
+}
+
---
+++ src/ILS/Valid.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ILS;
+/**
+ *
+ * @author Mithila Prabashwara
+ */
+
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+class InvalidEmailException extends Exception {
+    public InvalidEmailException(String message) {
+        super(message);
+    }
+}
+
+class InvalidLoginCredentialsException extends Exception {
+    public InvalidLoginCredentialsException(String message) {
+        super(message);
+    }
+}
+
+class InvalidPasswordException extends Exception {
+    public InvalidPasswordException(String message) {
+        super(message);
+    }
+}
+
+public class Valid {
+    //Used to check the format of the email addresses and if it is not in correct format throws a Exception
+    public static void formatEmail(String email) throws InvalidEmailException {
+        String format = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$";
+        if (!email.matches(format)) {
+            throw new InvalidEmailException("Please enter a valid email address.");
+        }
+    }
+    
+    //Used to check the login credentials and if the inputs are null or not in correct format throws a Exception
+    public static void login(String id, String pass) throws InvalidLoginCredentialsException {
+        if ((id == null || id.isEmpty()) && (pass == null || pass.isEmpty())) {throw new InvalidLoginCredentialsException("Please enter sid &  password.");}
+        else if (id == null || id.isEmpty()) { throw new InvalidLoginCredentialsException("Please enter a sid.");}
+        else if (pass == null || pass.isEmpty()) { throw new InvalidLoginCredentialsException("Please enter a password.");}
+        else if (!(id.length()==5)) { throw new InvalidLoginCredentialsException("Please enter a valid sid.");}
+    }
+    
+    
+    /**
+    public static void password(String pass) throws InvalidPasswordException {
+        if (pass == null || pass.isEmpty()) {
+            throw new InvalidPasswordException("Please enter a password.");
+        }
+    } **/
+}
